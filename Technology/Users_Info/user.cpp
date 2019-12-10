a8d9b59786ca8a75b58bf1e533d75386f86c10e8
atmshiblesadik <shiblesadik@outlook.com>
2019-12-10 23:44:10 +0600

    Update Settings and add marchant page

--- Technology/Users_Info/user.cpp
+++ Technology/Users_Info/user.cpp
@@ -1,10 +1,3 @@
 #pragma once
 #include "user.h"
 
-user_profile::user_profile(string full_name, string email_address, string password_plain_text) {
-
-}
-
-user_profile::~user_profile() {
-    
-}
--- Technology/Users_Info/user.h
+++ Technology/Users_Info/user.h
@@ -9,16 +9,9 @@ class user_profile {
 private:
     string private_key;
     string public_key;
-    string full_name;
-    string email_address;
-    string password_hash;
+    string wallet_id;
 public:
-    user_profile(string full_name, string email_address, string password_plain_text);
-    void set_private_key(string old_key, string new_key);
-    void set_public_key(string old_public_key, string new_public_key, string private_key);
-    string get_private_key_hash_value(string public_key);
     
-    ~user_profile();
 };
 
 #endif
